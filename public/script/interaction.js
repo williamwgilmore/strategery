1e8c94618e56349f29ed0d47fb181e5382736698
DESKTOP-SP83B60\willi <williamwgilmore@gmail.com>
2018-10-03 09:37:35 -0500

    Added ability to build towns

--- public/script/interaction.js
+++ public/script/interaction.js
@@ -1,18 +1,22 @@
+//Clicking on board space always deselects the last space and closes the build menu.
+//It can also open a build menu if a grass space is clicked
 $('.boardSpace').on('click', function(){
-	console.log($(this));
+	deselectSpace();
+	closeBuildMenu();
 	spaceType($(this));
-})
+});
 
-function spaceType(space){
-	showSpaceInfo(space);
-
-	if (space.hasClass('grass') || space.hasClass('buildable')){
-		openBuildMenu();
-	}
+//All functions called when clicking on a board space
+function deselectSpace(){ 
+	$('#board').find('.selectedSpace').removeClass('selectedSpace');
 }
 
-function openBuildMenu(){
-	
+function closeBuildMenu(){ 
+	$('#buildMenu').html('');
+}
+
+function selectSpace(space){ 
+	$(space).addClass('selectedSpace');
 }
 
 function showSpaceInfo(space){
@@ -25,3 +29,36 @@ function showSpaceInfo(space){
 	thisSpace = 'Row: ' + row + '</br>Col: ' + col + '</br>Type: ' + type; 
 	$('#spaceInfo').html(thisSpace);
 }
+
+function spaceType(space){
+	selectSpace(space);
+	showSpaceInfo(space);
+
+	if (space.hasClass('grass') || space.hasClass('buildable')){
+		openBuildMenu();
+	}
+}
+
+//Build functions
+function openBuildMenu(){
+	$('#buildMenu').html('<button class="buildTown" data-build="town">Build Town</button>');
+}
+
+function checkBuildCost(item){
+	switch (item){
+		case 'town':
+			return true;
+			break;
+		default:
+			return false;
+			break;
+	}
+}
+
+$('#buildMenu').on('click', '.buildTown', function() {
+	let space = $('#board').find('.selectedSpace');
+	let buildItem = $(this);
+	if (checkBuildCost(buildItem.data('build'))){
+		space.removeClass('grass').addClass('town');
+	}
+});
